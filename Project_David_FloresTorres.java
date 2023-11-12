5a8074dd03c691b3b9cea66e6caeca9f5d8223a2
soyDOFT <69359567+soyDOFT@users.noreply.github.com>
2023-11-11 23:14:58 -0500

    Update Project_David_FloresTorres.java
    Modified to read input from a file

--- Project_David_FloresTorres.java
+++ Project_David_FloresTorres.java
@@ -1,60 +1,69 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 class Project_David_FloresTorres
 {
-   public static void main(String[] args)
+   public static void main(String[] args) throws IOException
    {
-      double height,
-             weight;
-      int age;
-      String number, 
-             provider,
-             firstName,
-             lastName,
-             smokeStatus;
-      //Create object for user input
-      Scanner kb = new Scanner(System.in);
-      //Prompt user for insurance information and store it
-      System.out.print("Please enter the Policy Number: ");
-      number = kb.nextLine();
-      System.out.print("Please enter the Provider Name: ");
-      provider = kb.nextLine();
-      System.out.print("Please enter the Policyholder's First Name: ");
-      firstName = kb.nextLine();
-      System.out.print("Please enter the Policyholder's Last Name: ");
-      lastName = kb.nextLine();
-      System.out.print("Please enter the Policyholder's Age: ");
-      age = kb.nextInt();
-      //clear buffer
-      kb.nextLine();
-      System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-      smokeStatus = kb.nextLine().toLowerCase();
+      double height = 0.0,
+             weight = 0.0;
+      int age = 0,
+          totalSmokers = 0,
+          totalNonsmokers = 0;
+      String number = "", 
+             provider = "",
+             firstName = "",
+             lastName = "",
+             smokeStatus = "";
+             
+      //Create a Policy object to store client information
+      Policy client;
+      //Create an ArrayList to store clients
+      ArrayList<Policy> list = new ArrayList<>();
+      //Open the PolicyInformation.txt file
+      File file = new File("PolicyInformation.txt");
+      //Create a Scanner object to read the file
+      Scanner input = new Scanner(file);
       
-      //Validate user's smoking status
-      while (!smokeStatus.equals("smoker") && !smokeStatus.equals("non-smoker"))
+      //Loop through the file and extract information to ArrayList
+      while (input.hasNext())
       {
-         System.out.print("Please only enter smoker/non-smoker: ");
-         smokeStatus = kb.nextLine().toLowerCase();
+         number = input.nextLine();
+         provider = input.nextLine();;
+         firstName = input.nextLine();
+         lastName = input.nextLine();
+         age = input.nextInt();
+         
+         //clear buffer
+         input.nextLine();
+         
+         smokeStatus = input.nextLine();
+         height = input.nextDouble();
+         weight = input.nextDouble();
+         
+         //skip empty lines if there is more input
+         if (input.hasNext())
+         {
+            input.nextLine();
+            input.nextLine();
+         }
+         
+         //Store input information into Policy object
+         client = new Policy(height, weight, age, number, provider, firstName, lastName, smokeStatus);
+         //Add Policy instances to ArrayList
+         list.add(client);
       }
-      
-      System.out.print("Please enter the Policyholder's Height (in inches): ");
-      height = kb.nextDouble();
-      System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-      weight = kb.nextDouble();
-      
-      //Call object and initalize with Policyholder's information
-      Policy client = new Policy(height, weight, age, number, provider, firstName, lastName, smokeStatus);
-      
-      //Output all the information
-      System.out.print("\nPolicy Number: " + client.getNumber());
-      System.out.print("\nProvider Name: " + client.getProvider());
-      System.out.print("\nPolicyholder's First Name: " + client.getFirstName());
-      System.out.print("\nPolicyholder's Last Name: " + client.getLastName());
-      System.out.print("\nPolicyholder's Age: " + client.getAge());
-      System.out.print("\nPolicyholder's Smoking Status: " + client.getSmokeStatus());
-      System.out.printf("\nPolicyholder's Height: %,.1f inches", client.getHeight());
-      System.out.printf("\nPolicyholder's Weight: %,.1f pounds", client.getWeight());
-      System.out.printf("\nPolicyholder's BMI: %,.2f", client.getBMI());
-      System.out.printf("\nPolicy Price: $%,.2f", client.getFee());
+
+         System.out.print("\nPolicy Number: " + client.getNumber());
+         System.out.print("\nProvider Name: " + client.getProvider());
+         System.out.print("\nPolicyholder's First Name: " + client.getFirstName());
+         System.out.print("\nPolicyholder's Last Name: " + client.getLastName());
+         System.out.print("\nPolicyholder's Age: " + client.getAge());
+         System.out.print("\nPolicyholder's Smoking Status: " + client.getSmokeStatus());
+         System.out.printf("\nPolicyholder's Height: %,.1f inches", client.getHeight());
+         System.out.printf("\nPolicyholder's Weight: %,.1f pounds", client.getWeight());
+         System.out.printf("\nPolicyholder's BMI: %,.2f", client.getBMI());
+         System.out.printf("\nPolicy Price: $%,.2f\n\n", client.getFee());
+
    }
 }
